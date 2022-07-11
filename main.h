21dd947e3761481b87ea5825f827858820069c14
Motteo1 <timothykirungu@gmail.com>
2022-07-11 08:58:29 -0700

    Update

--- main.h
+++ main.h
@@ -4,11 +4,13 @@
 #include <stdarg.h> /* va_list */
 #include <stdlib.h> /* malloc, free */
 #include <unistd.h> /* write */
+
 /**
  * struct type - connect conversion specifiers with the correct print function
  * @print: a function pointer for the print functions
  * @identifier: the conversion specifier
  */
+
 typedef struct type
 {
 	char *identifier;
