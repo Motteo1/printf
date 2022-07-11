b5dfa3e512ce30e6f6d7d99ca0c9ce32e598efdd
bereket <emailbereket@gmail.com>
2022-07-11 06:37:43 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * printString - prints a string
+ * 
+ * Return: nothing
  */
 
 void printString(char *ch)
@@ -13,30 +13,4 @@ void printString(char *ch)
         putchar(*ch); /* print letter */
         ch = ch + 1;  /* point to the next letter. */
     }
-}
-
-#include "main.h"
-
-/**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
- */
-int _printf(const char *format, ...)
-{
-	int printed_chars;
-	conver_t f_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-	}
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
 }
