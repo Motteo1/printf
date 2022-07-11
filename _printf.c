725835c8810360f92f9812a490c799b7c863d22d
bereket <emailbereket@gmail.com>
2022-07-11 06:46:14 -0700

    task zero printf

--- _printf.c
+++ _printf.c
@@ -1,16 +1,51 @@
 #include "main.h"
 
 /**
- * printString - prints a string
- * 
- * Return: nothing
+ * _printf - print output to stdout according to a format string
+ * @format: the format of the string to print
+ *
+ * Return: number of characters printed (excluding null byte)
  */
 
-void printString(char *ch)
+int _printf(const char *format, ...)
 {
-    while (*ch != '\0')
-    {
-        putchar(*ch); /* print letter */
-        ch = ch + 1;  /* point to the next letter. */
-    }
+	int count = 0;
+	va_list args;
+	int (*function)(va_list) = NULL;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1) != '%')
+		{
+			format++;
+			function = get_function(format);
+			if (*(format) == '\0')
+				return (-1);
+			else if (function == NULL)
+			{
+				_putchar(*(format - 1));
+				_putchar(*format);
+				count += 2;
+			}
+			else
+				count += function(args);
+		}
+		else if (*format == '%' && *(format + 1) == '%')
+		{
+			format++;
+			_putchar('%');
+			count++;
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
